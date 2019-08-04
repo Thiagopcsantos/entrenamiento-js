ba04db19ceebfa9e3d3a03b2bc49bec5aef0dfcb
Thiago Cavalcante <thiagopcsantos@gmail.com>
2019-08-04 19:21:43 -0400

    commit juego js

---
+++ elSenorDeLosAnillos.js
@@ -0,0 +1,45 @@
+function Jugador(nombre){
+    this.nombre = nombre;
+    this.pv = 100;
+    this.sp = 100;
+
+    this.curar = function(jugadorObjetivo){
+
+        if(this.sp >=40){
+            this.sp -= 40;           
+            jugadorObjetivo.pv += 20;
+        }else{
+            console.info(this.nombre +  "no tiene sp");
+        }
+        this.estado(jugadorObjetivo);
+    }
+
+    this.tirarFlecha = function(jugadorObjetivo){
+
+        if(jugadorObjetivo > 40){
+            
+            jugadorObjetivo.pv -= 40;
+        }else{
+            jugadorObjetivo.pv = 0;
+            console.error( jugadorObjetivo.nombre + " murio!!!");
+        }
+
+        this.estado(jugadorObjetivo);
+    }
+
+    this.estado = function( jugadorObjetivo){
+        console.info( this);
+        console.info(jugadorObjetivo);
+    }
+}
+
+
+let gandalf = new Jugador("Gandalf");
+let legolas = new Jugador("Legolas");
+
+
+console.log(gandalf);
+console.log(legolas);
+
+gandalf.curar(legolas);
+
