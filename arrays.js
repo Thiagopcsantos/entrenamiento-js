4e275c3d032882837071b9bc6331a515a36ded5c
Thiago Cavalcante <thiagopcsantos@gmail.com>
2019-08-15 12:21:25 -0400

    commit arrays1

--- arrays.js
+++ arrays.js
@@ -1,7 +1,62 @@
 
 
-
-
-let arr = [1,2,3,5,6,7];
+let arr = [1,2,3,4,5];
 
 console.log(arr);
+
+console.log ( arr[0]);
+
+console.log( arr[3]);
+
+arr.reverse();
+console.log(arr);
+
+
+arr = arr.map(function(elem){
+    elem *= elem;
+    return elem;
+});
+
+console.log(arr);
+
+
+arr = arr.map(Math.sqrt);
+console.log(arr);
+
+
+// arr = arr.join();
+// console.log(arr);
+
+
+// arr = arr.split();
+// console.log(arr);
+
+
+
+arr.reverse();
+console.log(arr);
+
+arr.push(6, 7,8);
+console.log(arr);
+
+arr.unshift(0);
+console.log(arr);
+
+
+console.log(arr.toString() );
+
+
+let elimine = arr.pop();
+console.log( arr, elimine);
+
+arr.splice( 1, 2, 22, 33);
+console.log(arr);
+
+
+
+arr = arr.slice(0,5);
+ console.log(arr);
+
+
+
+
--- treinamento.js
+++ treinamento.js
@@ -23,50 +23,68 @@
 // console.log(maxi.imprimirFilhote());
 
 
-function Jugador(nombre){
-    this.nombre = nombre;
-    this.puntosdevida = 100;
-    this.special = 100;
+// function Jugador(nombre){
+//     this.nombre = nombre;
+//     this.puntosdevida = 100;
+//     this.special = 100;
 
-    this.curar = function(jugadorPrincipal){
+//     this.curar = function(jugadorPrincipal){
 
-        if(this.special >= 20){
-            this.special -20;
-            jugadorPrincipal.puntosdevida += 10;
-        }else{
-            console.info(this.nombre + "No tiene Especial");
-        }
-        this.estado(jugadorPrincipal);
-    }
+//         if(this.special >= 20){
+//             this.special -20;
+//             jugadorPrincipal.puntosdevida += 10;
+//         }else{
+//             console.info(this.nombre + "No tiene Especial");
+//         }
+//         this.estado(jugadorPrincipal);
+//     }
 
-    this.darUmTiro = function(jugadorPrincipal){
-        if(jugadorPrincipal > 45){
-            jugadorPrincipal.puntosdevida -= 45;
-        }else{
-            jugadorPrincipal.puntosdevida = 0;
-            console.error( jugadorPrincipal.nombre + "Murrio ")
-        }
-        this.estado(jugadorPrincipal);
-    }
-    this.estado = function(jugadorPrincipal){
-        console.info(this);
-        console.info(jugadorPrincipal);
-    } 
-}
+//     this.darUmTiro = function(jugadorPrincipal){
+//         if(jugadorPrincipal > 45){
+//             jugadorPrincipal.puntosdevida -= 45;
+//         }else{
+//             jugadorPrincipal.puntosdevida = 0;
+//             console.error( jugadorPrincipal.nombre + "Murrio ")
+//         }
+//         this.estado(jugadorPrincipal);
+//     }
+//     this.estado = function(jugadorPrincipal){
+//         console.info(this);
+//         console.info(jugadorPrincipal);
+//     } 
+// }
 
-let papaleguas = new Jugador("Papaleguas");
-let coiote = new Jugador ("Coiote");
-
-
-console.log(papaleguas);
-console.log(coiote);
+// let papaleguas = new Jugador("Papaleguas");
+// let coiote = new Jugador ("Coiote");
 
 
-papaleguas.curar(coiote);
+// console.log(papaleguas);
+// console.log(coiote);
+
+
+// papaleguas.curar(coiote);
 
 
 
+let arr = [1,2,3,4,5,6,7,8,9];
 
+
+console.log(arr);
+
+
+arr.reverse();
+console.log(arr);
+
+
+arr = arr.map(function(elem){
+    elem *= elem;
+    return elem;
+});
+
+console.log(arr);
+
+arr.reverse();
+console.log(arr);
 
 
 
